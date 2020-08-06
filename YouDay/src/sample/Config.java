9fc22a1333d88f4432a4ca29900995ee88902147
Gelnas <47691659+Gelnas@users.noreply.github.com>
2020-08-06 17:33:52 +0400

    Update Config.java
    delete my token

--- YouDay/src/sample/Config.java
+++ YouDay/src/sample/Config.java
@@ -3,7 +3,7 @@ package sample;
 import com.dropbox.core.DbxRequestConfig;
 
 public class Config {
-    public static String TOKEN = "3h5KwXERRHAAAAAAAAAARRdQbxkNashPY7zNupJnFNoBssV1qJw-enBy1ofpDc-D";
+    public static String TOKEN = "XXX";
     public static DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
     protected String bdHost = "localhost";
     protected String bdPort = "3310";
